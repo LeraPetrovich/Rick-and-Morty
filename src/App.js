f06c54c73666917ea92599806ae9e78ef80f8efa
Valeria <lerapetrovic8@gmail.com>
2022-05-29 18:53:03 +0300

    feat: filter

--- src/App.js
+++ src/App.js
@@ -9,15 +9,15 @@ import Search from "./components/Search/Search";
 import styles from './components/Card/Card.module.scss';
 function App() {
   let [pageNumber,setPageNumber]=useState(1); //состояние влияет на переключение имен и самих карточек
-  let[fetchedData, updateFetchedData]=useState([]);
-  let {info, results} =fetchedData;
+  let[fetchedData, updateFetchedData]=useState([]);  //состояние карт
+  let {info, results} =fetchedData; 
   let [search, setSearch]=useState("")
   let [status, updateStatus] = useState("");
 let [gender, updateGender] = useState("");
 let [species, updateSpecies] = useState("");
-  console.log(results);
+  //console.log(results);
   let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}&name=${search}&status=${status}&gender=${gender}&species=${species}`;
-  useEffect(()=>{
+  useEffect(()=>{   //информация из api
     (async function(){
       let data =await fetch(api).then(res=>res.json());
       updateFetchedData(data);
@@ -32,7 +32,7 @@ let [species, updateSpecies] = useState("");
 <div className="container ">
 <div className="row">
 <Filters
-pageNumber={pageNumber}
+  pageNumber={pageNumber}
   status={status}
   updateStatus={updateStatus}
   updateGender={updateGender}
@@ -46,7 +46,7 @@ pageNumber={pageNumber}
 </div>
 </div>
 </div>
-<Pagination info={info} setPageNumber={setPageNumber}/>
+<Pagination info={info} setPageNumber={setPageNumber} pageNumber={pageNumber}/>
 </div>
 );
 }
